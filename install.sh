0f4181383ce4d300f96d66c7c605e6bc75bbde30
Jamie Wilkinson <jamie@jamiedubs.com>
2011-02-16 11:29:48 -0800

    Ghetto installer script (uses /usr/local/bin)

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+here=$(pwd -P)
+tmp="/tmp/git-friendly"
+
+git clone git://github.com/jamiew/git-friendly.git $tmp >/dev/null 2>&1
+rm -rf $tmp/.git
+rm -f $tmp/README* $tmp/install.sh
+cp $tmp/* /usr/local/bin/
+
+echo
+echo "Done! Try typing 'pull' in a git repository to get started"
+echo "For more info visit http://github.com/jamiew/git-friendly"
+echo
+
