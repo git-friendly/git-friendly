10b41be44ac99f7a3fdd3aa9656d97986ffd7e51
John Manoogian III <jm3@jm3.net>
2011-03-17 10:57:47 -0700

    fix issue 6, installer should handle install fails

--- install.sh
+++ install.sh
@@ -2,15 +2,27 @@
 
 here=$(pwd -P)
 tmp="/tmp/git-friendly"
+dest="/usr/local/bin"
 
 git clone git://github.com/jamiew/git-friendly.git $tmp >/dev/null 2>&1
 rm -rf $tmp/.git
 rm -f $tmp/README* $tmp/install.sh
-cp $tmp/* /usr/local/bin/
+installed_scripts=`ls -1 ${tmp}`
+cp $tmp/* ${dest}/ &> /dev/null
 rm -rf $tmp
+for s in ${installed_scripts}; do
+  if [ ! -f ${dest}/${s} ]; then
+    echo
+    echo "Oops! The ${s} command couldn't be installed, installation failed."
+    echo "Please re-run me as someone who can install into ${dest}."
+    echo
+    exit 1
+  fi
+done
 
 echo
-echo "Done! Try typing 'pull' in a git repository to get started"
+echo "Done! Try typing 'pull' in any git repository to get started"
 echo "For more info visit http://github.com/jamiew/git-friendly"
 echo
 
+exit 0
